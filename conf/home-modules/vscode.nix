72e264037648819c3bf2adac4bbcd7073c1c006d
abkein <rickbatra0z@gmail.com>
2025-06-07 04:45:18 +0300

    VSCode snippets

--- conf/home-modules/vscode.nix
+++ conf/home-modules/vscode.nix
@@ -451,5 +451,92 @@ in
       "nixEnvSelector.suggestion" = true;
 
     };
+    globalSnippets = {
+      Shebang = {
+        scope = "bash, python";
+        prefix = "#!";
+        body = [
+          "#!/usr/bin/env $1"
+          "\${2:# -*- coding: utf-8 -*-}"
+        ];
+        description = "Paste shebang";
+      };
+    };
+    languageSnippets = {
+      python = {
+        plot = {
+          prefix = [ "plt.plot" ];
+          body = [
+            "fig = plt.figure()"
+            "ax1 = fig.add_subplot(1,1,1)"
+            "line11, = ax1.plot($1)"
+            ""
+            "#  fig.savefig(\"\", bbox_inches=\"tight\", transparent=True)"
+            "plt.show()"
+          ];
+          description = "Matplotlib extended plot";
+        };
+        scatter = {
+          prefix = [ "plt.scatter" ];
+          body = [
+            "fig = plt.figure()"
+            "ax1 = fig.add_subplot(1,1,1)"
+            "line11 = ax1.scatter($1)"
+            ""
+            "#  fig.savefig(\"\", bbox_inches=\"tight\", transparent=True)"
+            "plt.show()"
+          ];
+          description = "Matplotlib extended plot, scatter";
+        };
+	      ignore_type = {
+	      	prefix = "# t";
+	      	body = [
+	      		"# type: ignore"
+	      	];
+	      	description = "Inserts type ignore instruction";
+	      };
+        passifmain = {
+          prefix = "if __";
+          body = [
+            "if __name__ == \"__main__\":"
+            "    pass"
+            ""
+          ];
+          description = "Pass if main";
+        };
+      };
+      cpp = {
+        print = {
+          prefix = "std::c";
+          body = [
+            "std::cout << $1 << std::endl;"
+          ];
+          description = "Output to console";
+        };
+      };
+      latex = {
+        text = {
+          prefix = "\\t";
+          body = [
+            "\\text{$1}"
+          ];
+          description = "\\text{}";
+        };
+        limit = {
+          prefix = "\\lim";
+          body = [
+            "\\lim\\limits_{\\substack{\${1:a} \\to -\${2:\\infty} \${3:b} \\to \${4:\\infty}}}"
+          ];
+          description = "Double limits";
+        };
+        therm_deriv = {
+          prefix = "\\lfp";
+          body = [
+            "\\left(\\frac{\\partial $1}{\\partial $2}\\right)_{$3}"
+          ];
+          description = "Thermodynamic derivative";
+        };
+      };
+    };
   };
 }
