0205f92c2da428baced48a594b35ea388d5cc0b9
abkein <rickbatra0z@gmail.com>
2025-06-20 00:25:49 +0300

    Added .gitignore

--- conf/home-modules/vscode/declared_workspaces.nix
+++ conf/home-modules/vscode/declared_workspaces.nix
@@ -1,8 +1,39 @@
-{config, pkgs}:
+{ config, lib, pkgs, options, ... }:
 let
-  declare_workspace = import ./declare_workspace.nix {config=config; pkgs=pkgs;};
+  declare_workspace = { name, folder, settings, prerun ? "", postrun ? "", preinit ? true }: rec {
+    configFile.${name} = {
+      enable = true;
+      executable = false;
+      force = true;
+      target = "vscode_workspaces/${name}.code-workspace";
+      text = builtins.toJSON {
+        folders = [{ path = folder; }];
+        settings = settings;
+      };
+    };
+    desktopEntries."CodeWSpaceSelector".actions.${name} =
+    let
+      init_cmd = "nix-shell ${folder}/shell.nix --command \"exit\"";
+      init_wrap_cmd = "kitty --app-id=\"kitty_info\" ${init_cmd}";
+      prefix = if (prerun == "") then (if preinit then "${init_wrap_cmd} &&" else "") else "${prerun} && ";
+      postfix = if (postrun == "") then "" else " && ${postrun}";
+      codecmd = "${pkgs.vscode-fhs}/bin/code --password-store=gnome-libsecret --ozone-platform=wayland ${config.xdg.configHome}/${configFile.${name}.target}";
+      cmd = "${prefix}${codecmd}${postfix}";
+    in
+    {
+      name = "${name}";
+      exec = "bash -c \"${cmd}\"";
+    };
+  };
 in
 [
+  mkMerge (builtins.attrValues (lib.foldl' (acc: spec: acc // mkConfFile spec)
+  {}
+  [
+    { name = "proj1"; folder = "/path/to/p1"; settings = { /*…*/ }; }
+    { name = "proj2"; folder = "/path/to/p2"; settings = { /*…*/ }; }
+    { name = "proj3"; folder = "/path/to/p3"; settings = { /*…*/ }; }
+  ]))
   (declare_workspace {
     name = "configuration";
     folder = "${config.home.homeDirectory}/nixos-configuration";
