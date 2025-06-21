4c977a8ee9869c8c250bc04dde0f741465101c33
abkein <rickbatra0z@gmail.com>
2025-06-21 12:23:42 +0300

    ...

--- conf/home-modules/vscode/declare_workspace.nix
+++ conf/home-modules/vscode/declare_workspace.nix
@@ -2,26 +2,21 @@
 config: name: spec: rec {
   configFile."${name}" =
   let
-  #   allow = if spec.extension_management_policy == "whitelist" then true else false;
-  #   allowed_exts = foldl' (acc: ext: acc // {"${ext}" = true;}) {} cfg.always_allowed_extensions;
-  #   exts = allowed_exts // (foldl' (acc: ext: acc // {"${ext}" = allow;}) {} spec.extensions) // {"*" = !allow;};
-    # profSet = if spec.profile == "" then {} else { "workbench.profile" = spec.profile; };
     envSet = if spec.hasShell then {
       "nixEnvSelector.suggestion" = false;
       "nixEnvSelector.nixFile" = "\${workspaceFolder}/shell.nix";
     } else {};
     settings = envSet // spec.settings;
-    conf = {
-      folders = [{ path = spec.folder; }];
-      settings = settings;
-    } // (if spec.profile == "" then {} else { profiles.source = spec.profile;});
   in
   {
     enable     = true;
     executable = false;
     force      = true;
     target     = "vscode_workspaces/${name}.code-workspace";
-    text       = builtins.toJSON conf;
+    text       = builtins.toJSON {
+      folders = [{ path = spec.folder; }];
+      settings = settings;
+    };
   };
 
   desktopEntries."CodeWorkspaceSelector".actions."${name}" =
@@ -31,6 +26,7 @@ config: name: spec: rec {
     prefix      = if spec.prerun != "" then "${spec.prerun} && "
                   else if (spec.preinit && spec.hasShell) then "${initWrap} && " else "";
     postfix     = if spec.postrun != "" then " && ${spec.postrun}" else "";
+    profileCmd  = if spec.profile == "" then "" else "--profile ${spec.profile}";
     codeCmd     = "${config.better-code.code-package}/bin/code --password-store=gnome-libsecret --ozone-platform=wayland ${config.xdg.configHome}/${configFile.${name}.target}";
     fullCommand = "${prefix}${codeCmd}${postfix}";
   in {
