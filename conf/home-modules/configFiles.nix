6451bac6336fbde6e9793ea11aac7c4c0e67b228
abkein <rickbatra0z@gmail.com>
2025-06-24 20:58:09 +0300

    Hyprlock

--- conf/home-modules/configFiles.nix
+++ conf/home-modules/configFiles.nix
@@ -36,6 +36,19 @@ in {
     source = ../confs/python/simple.ipynb;
     target = "python/simple.ipynb";
   };
+  hyprlock-te = generic // {
+    target = "hyprlock/te.markup";
+    text = ''
+      <markup>
+        <span color="#E2E2E2" bgcolor="#2980B9" bgalpha="50%" face="Gabarito" size="20pt">
+          Alles muss sich veraendern
+        </span>
+        <span color="#E2E2E2" bgcolor="#2980B9" bgalpha="50%" face="Gabarito" size="20pt">
+          or no
+        </span>
+      </markup>
+    '';
+  };
   # hyprland-regreet = generic // {
   #   source = ../confs/Hyprland-regreet.conf;
   #   target = "hypr/hyprland-regreet.conf";
