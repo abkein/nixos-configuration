7d3bd2fbd8c7c2bac89a35af45ab17a5bdfb3ba6
abkein <rickbatra0z@gmail.com>
2025-06-24 19:33:28 +0300

    Hyprpaper

--- conf/home-manager.nix
+++ conf/home-manager.nix
@@ -103,6 +103,21 @@
       configFile = "${config.xdg.configHome}/dunst/dunstrc";
       settings = import ./home-modules/dunst.nix;
     };
+    hyprpaper = {
+      enable = true;
+      settings = {
+        preload = [
+          "~/Pictures/Wallpapers/sr-71-red_copy.png"
+          "~/Pictures/Wallpapers/sr-71-black-side_copy.jpg"
+          "~/Pictures/Wallpapers/rocket_copy.png"
+        ];
+        wallpaper = [
+          "eDP-1, ~/Pictures/Wallpapers/rocket_copy.png"
+          "DP-6, ~/Pictures/Wallpapers/sr-71-black-side_copy.jpg"
+          "DP-9, ~/Pictures/Wallpapers/sr-71-red_copy.png"
+        ];
+      };
+    };
     # syncthing = {
     #   enable = true;
     #   openDefaultPorts = true;
