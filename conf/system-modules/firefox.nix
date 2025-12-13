22cf651b7f8c8a5fcc9b2c7349a4a21e2c2d37a6
abkein <rickbatra0z@gmail.com>
2025-12-13 10:32:40 +0300

    Firefox extensions

--- conf/system-modules/firefox.nix
+++ conf/system-modules/firefox.nix
@@ -8,17 +8,17 @@
   # https://mozilla.github.io/policy-templates/
   policies = let
     makeExt = id: {
-      ${id} = {
-        install_url =
-          "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+      name = id;
+      value = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
         installation_mode = "force_installed";
         default_area = "menupanel";
       };
     };
     makeExtNav = id: {
-      ${id} = {
-        install_url =
-          "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+      name = id;
+      value = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
         installation_mode = "force_installed";
         default_area = "navbar";
       };
@@ -43,33 +43,49 @@
     ExtensionUpdate = true;
     # ---- EXTENSIONS ----
     # Check about:support for extension/add-on ID strings.
-    ExtensionSettings = lib.mkMerge [
+    ExtensionSettings =
       {
         # "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
       }
-      (makeExtNav "easyscreenshot@mozillaonline.com") # Easy Screenshot
-      (makeExtNav "simple-tab-groups@drive4ik") # Simple Tab Groups
-      (makeExtNav "@testpilot-containers") # Firefox Multi-Account Containers
-      (makeExtNav "keepassxc-browser@keepassxc.org") # KeePassXC-Browser
-      (makeExtNav "addon@darkreader.org") # Dark Reader
-      (makeExtNav "zotero@chnm.gmu.edu") # Zotero Connector
-
-      (makeExt "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}") # Auto Tab Discard
-      (makeExt "jid1-BoFifL9Vbdl2zQ@jetpack") # Decentraleyes
-      (makeExt
-        "firefox.container-shortcuts@strategery.io") # Easy Container Shortcuts
-      (makeExt "{1018e4d6-728f-4b20-ad56-37578a4de76b}") # Flagfox
-      (makeExt "jid1-AQqSMBYb0a8ADg@jetpack") # Mailvelope
-      (makeExt "jid1-MnnxcxisBPnSXQ@jetpack") # Privacy Badger
-      (makeExt "{2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c}") # Search by Image
-      (makeExt "uBlock0@raymondhill.net") # uBlock Origin
-      (makeExt "{b9db16a4-6edc-47ec-a1f4-b86292ed211d}") # Video DownloadHelper
-      (makeExt "rto@rto.rto") # РуТрекер - официальный плагин (доступ и пр.)
-      # (makeExt "{73a6fe31-595d-460b-a920-fcc0f8843232}") # NoScript
-      # (makeExt "suziwen1@gmail.com") # Proxy SwitchyOmega 3 (ZeroOmega)
-      # (makeExt "{cb08faed-9460-474a-ba0b-d98b13b5e001}") # Regex Search
-      # (makeExt "{e662576a-2f73-4069-bcca-ddf440fea62b}") # Web Apps by 123apps
-    ];
+      // builtins.listToAttrs (builtins.map makeExtNav
+      [
+        "easyscreenshot@mozillaonline.com" # Easy Screenshot
+        "simple-tab-groups@drive4ik" # Simple Tab Groups
+        "@testpilot-containers" # Firefox Multi-Account Containers
+        "keepassxc-browser@keepassxc.org" # KeePassXC-Browser
+        "addon@darkreader.org" # Dark Reader
+        "zotero@chnm.gmu.edu" # Zotero Connector
+        "button@scholar.google.com"  # Google Scholar Button
+        "{6031c27b-5ae2-4449-a7fd-ac7feabb4ef3}"  # Sci-Hub
+        "{0e10f3d7-07f6-4f12-97b9-9b27e07139a5}"  # Netcraft Extension
+      ]
+      ) // builtins.listToAttrs (builtins.map makeExt
+      [
+        "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}" # Auto Tab Discard
+        "jid1-BoFifL9Vbdl2zQ@jetpack" # Decentraleyes
+        "firefox.container-shortcuts@strategery.io" # Easy Container Shortcuts
+        "{1018e4d6-728f-4b20-ad56-37578a4de76b}" # Flagfox
+        "jid1-AQqSMBYb0a8ADg@jetpack" # Mailvelope
+        "jid1-MnnxcxisBPnSXQ@jetpack" # Privacy Badger
+        "{2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c}" # Search by Image
+        "uBlock0@raymondhill.net" # uBlock Origin
+        "{b9db16a4-6edc-47ec-a1f4-b86292ed211d}" # Video DownloadHelper
+        "rto@rto.rto" # РуТрекер - официальный плагин (доступ и пр.)
+        "@amiunique-extension"  # AmIUnique
+        "{96ef5869-e3ba-4d21-b86e-21b163096400}"  #Font Fingerprint Defender
+        "queryamoid@kaply.com"  # Query AMO Addon ID
+        "{a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7}"  # User-Agent Switcher and Manager
+        # "{2cf5dbed-78fe-4bd5-9524-38fdf837be98}"  # WebGL Fingerprint Defender
+        # "{55f61747-c3d3-4425-97f9-dfc19a0be23c}"  # Spoof Timezone
+        # "CanvasBlocker@kkapsner.de"  # CanvasBlocker  # Breaks certain websites
+        # "2.0@disconnect.me"  # Disconnect  # Breaks certain websites
+        # "jid1-ZAdIEUB7XOzOJw@jetpack"  # DuckDuckGo Privacy Essentials  # Breaks certain websites
+        # "{73a6fe31-595d-460b-a920-fcc0f8843232}" # NoScript
+        # "suziwen1@gmail.com" # Proxy SwitchyOmega 3 (ZeroOmega)
+        # "{cb08faed-9460-474a-ba0b-d98b13b5e001}" # Regex Search
+        # "{e662576a-2f73-4069-bcca-ddf440fea62b}" # Web Apps by 123apps
+      ]
+      );
     HardwareAcceleration = true;
     HttpsOnlyMode = "force_enabled";
     OfferToSaveLoginsDefault = false;
