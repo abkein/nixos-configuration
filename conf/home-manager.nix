e520f07e2574326ade87ff9893dd2ba82a9b32d8
abkein <rickbatra0z@gmail.com>
2026-01-17 16:51:31 +0300

    Disabled anyrun, b/c not working (panic, core dump) and fetches smth from internet at each rebuild

--- conf/home-manager.nix
+++ conf/home-manager.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, ... }:
 let
   ayugram-desktop = inputs.ayugram-desktop.packages.${pkgs.system}.ayugram-desktop;
-  anyrun-pkgs = inputs.anyrun.packages.${pkgs.system};
+  # anyrun-pkgs = inputs.anyrun.packages.${pkgs.system};
 in
 {
   imports = [
@@ -246,55 +246,55 @@ in
       };
     };
     git-credential-oauth = { enable = false; };
-    anyrun = {
-      enable = true;
-      config = {
-        x = { fraction = 0.5; };
-        y = { fraction = 0.3; };
-        width = { fraction = 0.3; };
-        hideIcons = false;
-        ignoreExclusiveZones = false;
-        layer = "overlay";
-        hidePluginInfo = false;
-        closeOnClick = false;
-        showResultsImmediately = false;
-        maxEntries = null;
-        plugins = with anyrun-pkgs; [
-          # An array of all the plugins you want, which either can be paths to the .so files, or their packages
-          applications  #
-          dictionary    # :def
-          shell         # :sh
-          rink
-          symbols
-          websearch     # ?
-        ];
-      };
-      extraConfigFiles =
-      {
-        "applications.ron".text = ''
-          Config(
-            // Also show the Desktop Actions defined in the desktop files, e.g. "New Window" from LibreWolf
-            desktop_actions: true,
+    # anyrun = {
+    #   enable = true;
+    #   config = {
+    #     x = { fraction = 0.5; };
+    #     y = { fraction = 0.3; };
+    #     width = { fraction = 0.3; };
+    #     hideIcons = false;
+    #     ignoreExclusiveZones = false;
+    #     layer = "overlay";
+    #     hidePluginInfo = false;
+    #     closeOnClick = false;
+    #     showResultsImmediately = false;
+    #     maxEntries = null;
+    #     plugins = with anyrun-pkgs; [
+    #       # An array of all the plugins you want, which either can be paths to the .so files, or their packages
+    #       applications  #
+    #       dictionary    # :def
+    #       shell         # :sh
+    #       rink
+    #       symbols
+    #       websearch     # ?
+    #     ];
+    #   };
+    #   extraConfigFiles =
+    #   {
+    #     "applications.ron".text = ''
+    #       Config(
+    #         // Also show the Desktop Actions defined in the desktop files, e.g. "New Window" from LibreWolf
+    #         desktop_actions: true,
 
-            max_entries: 50,
+    #         max_entries: 50,
 
-            // A command to preprocess the command from the desktop file. The commands should take arguments in this order:
-            // command_name <term|no-term> <command>
-            // preprocess_exec_script: Some("/home/user/.local/share/anyrun/preprocess_application_command.sh")
+    #         // A command to preprocess the command from the desktop file. The commands should take arguments in this order:
+    #         // command_name <term|no-term> <command>
+    #         // preprocess_exec_script: Some("/home/user/.local/share/anyrun/preprocess_application_command.sh")
 
-            // The terminal used for running terminal based desktop entries, if left as `None` a static list of terminals is used
-            // to determine what terminal to use.
-            terminal: Some(Terminal(
-              // The main terminal command
-              command: "kitty",
-              // What arguments should be passed to the terminal process to run the command correctly
-              // {} is replaced with the command in the desktop entry
-              args: "--hold --app-id="kitty_quick" {}",
-            )),
-          )
-        '';
-      };
-    };
+    #         // The terminal used for running terminal based desktop entries, if left as `None` a static list of terminals is used
+    #         // to determine what terminal to use.
+    #         terminal: Some(Terminal(
+    #           // The main terminal command
+    #           command: "kitty",
+    #           // What arguments should be passed to the terminal process to run the command correctly
+    #           // {} is replaced with the command in the desktop entry
+    #           args: "--hold --app-id="kitty_quick" {}",
+    #         )),
+    #       )
+    #     '';
+    #   };
+    # };
     # texlive = {
     #   enable = true;
     #   package = pkgs.texlive.combine pkgs.texlive.combined.scheme-full;
