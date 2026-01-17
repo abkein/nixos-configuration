6219add51576266482d131f20f0ebd491d97ad54
abkein <rickbatra0z@gmail.com>
2026-01-17 16:49:43 +0300

    Added simple openssh conf to test IPv6 connectivity

--- conf/system-modules/openssh.nix
+++ conf/system-modules/openssh.nix
@@ -2,25 +2,25 @@
 {
   services.openssh = {
     enable = true;
-    listenaddress = [
+    listenAddresses = [
       {
-        addr = "fdcd:59e2:3ff3:0:263c:ed4a:7037:f1bb";
+        addr = "[fdcd:59e2:3ff3:0:263c:ed4a:7037:f1bb]";
         port = 22;
       }
       {
-        addr = "fdcd:59e2:3ff3:0:ca10:8ba0:9523:f762";
+        addr = "[fdcd:59e2:3ff3:0:ca10:8ba0:9523:f762]";
         port = 22;
       }
       {
-        addr = "2a03:d000:4191:485c:4698:359a:8e34:30ae";
+        addr = "[2a03:d000:4191:485c:4698:359a:8e34:30ae]";
         port = 22;
       }
       {
-        addr = "2a03:d000:4191:485c:1298:9f52:af8b:b03";
+        addr = "[2a03:d000:4191:485c:1298:9f52:af8b:b03]";
         port = 22;
       }
       {
-        addr = "fe80::33c3:37a2:d653:5fdf";
+        addr = "[fe80::33c3:37a2:d653:5fdf]";
         port = 22;
       }
     ];
