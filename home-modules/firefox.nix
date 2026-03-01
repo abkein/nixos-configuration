ac894dd9d87414480fed8377ff99b5db7d3f2a53
abkein <rickbatra0z@gmail.com>
2026-03-01 19:10:01 +0300

    Firefox

--- home-modules/firefox.nix
+++ home-modules/firefox.nix
@@ -1,4 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  makeExt = id: {
+    name = id;
+    value = {
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+      installation_mode = "force_installed";
+      default_area = "menupanel";
+    };
+  };
+  makeExtNav = id: {
+    name = id;
+    value = {
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
+      installation_mode = "force_installed";
+      default_area = "navbar";
+    };
+  };
+  L2A = func: lst: builtins.listToAttrs (builtins.map func lst);
+in
 {
   programs.firefox = {
     enable = true;
@@ -12,7 +31,11 @@
       # gpgme.dev
       # gpgme.info
     ];
-    languagePacks = [ "en-US" "ru" "de" ];
+    languagePacks = [
+      "en-US"
+      "ru"
+      "de"
+    ];
     profiles.default = {
       id = 0;
       name = "default";
@@ -65,31 +88,19 @@
           icon = "circle";
         };
       };
+      extensions = {
+        packages = with pkgs.nur.repos.rycee.firefox-addons; [
+          zotero-connector
+        ];
+      };
     };
     # https://mozilla.github.io/policy-templates/
-    policies = let
-      makeExt = id: {
-        name = id;
-        value = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
-          installation_mode = "force_installed";
-          default_area = "menupanel";
-        };
-      };
-      makeExtNav = id: {
-        name = id;
-        value = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/${id}/latest.xpi";
-          installation_mode = "force_installed";
-          default_area = "navbar";
-        };
-      };
-    in {
+    policies = {
       AutofillCreditCardEnabled = false;
       CaptivePortal = true;
       DNSOverHTTPS = {
         Enabled = true;
-        ProviderURL = "https://8.8.8.8/dns-query";  # 8.8.8.8, 8.8.4.4
+        ProviderURL = "https://8.8.8.8/dns-query"; # 8.8.8.8, 8.8.4.4
         # ProviderURL = "https://mozilla.cloudflare-dns.com/dns-query";  # 162.159.61.4 172.64.41.4
         # ProviderURL = "https://dns.cloudflare.com/dns-query";  # 162.159.61.4 172.64.41.4
         # ProviderURL = " https://dns.quad9.net/dns-query";  # 9.9.9.9, 149.112.112.112
@@ -97,62 +108,78 @@
         ExcludedDomains = [ ];
         Fallback = false;
       };
+      DisableAppUpdate = true;
+      DisableSetDesktopBackground = true;
       EnableTrackingProtection = {
         Value = true;
         Locked = true;
         Cryptomining = true;
         Fingerprinting = true;
+        EmailTracking = true;
+        SuspectedFingerprinting = true;
+        Category = "strict";
+        Exceptions = [ ];
+        BaselineExceptions = true;
+        ConvenienceExceptions = true;
+      };
+      FirefoxHome = {
+        Search = true;
+        TopSites = true;
+        SponsoredTopSites = false;
+        Highlights = false;
+        Pocket = true;
+        Stories = false;
+        SponsoredPocket = false;
+        SponsoredStories = false;
+        Snippets = true;
+        Locked = true;
       };
       ExtensionUpdate = true;
       # ---- EXTENSIONS ----
       # Check about:support for extension/add-on ID strings.
-      ExtensionSettings =
-        {
-          # "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
-        }
-        // builtins.listToAttrs (builtins.map makeExtNav
-        [
-          "easyscreenshot@mozillaonline.com" # Easy Screenshot
-          "simple-tab-groups@drive4ik" # Simple Tab Groups
-          "@testpilot-containers" # Firefox Multi-Account Containers
-          "keepassxc-browser@keepassxc.org" # KeePassXC-Browser
-          "addon@darkreader.org" # Dark Reader
-          "zotero@chnm.gmu.edu" # Zotero Connector
-          "button@scholar.google.com"  # Google Scholar Button
-          "{6031c27b-5ae2-4449-a7fd-ac7feabb4ef3}"  # Sci-Hub
-          "{0e10f3d7-07f6-4f12-97b9-9b27e07139a5}"  # Netcraft Extension
-        ]
-        ) // builtins.listToAttrs (builtins.map makeExt
-        [
-          "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}" # Auto Tab Discard
-          "jid1-BoFifL9Vbdl2zQ@jetpack" # Decentraleyes
-          "firefox.container-shortcuts@strategery.io" # Easy Container Shortcuts
-          "{1018e4d6-728f-4b20-ad56-37578a4de76b}" # Flagfox
-          "jid1-AQqSMBYb0a8ADg@jetpack" # Mailvelope
-          "jid1-MnnxcxisBPnSXQ@jetpack" # Privacy Badger
-          "{2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c}" # Search by Image
-          "uBlock0@raymondhill.net" # uBlock Origin
-          "{b9db16a4-6edc-47ec-a1f4-b86292ed211d}" # Video DownloadHelper
-          "rto@rto.rto" # РуТрекер - официальный плагин (доступ и пр.)
-          "@amiunique-extension"  # AmIUnique
-          "{96ef5869-e3ba-4d21-b86e-21b163096400}"  #Font Fingerprint Defender
-          "queryamoid@kaply.com"  # Query AMO Addon ID
-          "{a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7}"  # User-Agent Switcher and Manager
-          # "{2cf5dbed-78fe-4bd5-9524-38fdf837be98}"  # WebGL Fingerprint Defender
-          # "{55f61747-c3d3-4425-97f9-dfc19a0be23c}"  # Spoof Timezone
-          # "CanvasBlocker@kkapsner.de"  # CanvasBlocker  # Breaks certain websites
-          # "2.0@disconnect.me"  # Disconnect  # Breaks certain websites
-          # "jid1-ZAdIEUB7XOzOJw@jetpack"  # DuckDuckGo Privacy Essentials  # Breaks certain websites
-          # "{73a6fe31-595d-460b-a920-fcc0f8843232}" # NoScript
-          # "suziwen1@gmail.com" # Proxy SwitchyOmega 3 (ZeroOmega)
-          # "{cb08faed-9460-474a-ba0b-d98b13b5e001}" # Regex Search
-          # "{e662576a-2f73-4069-bcca-ddf440fea62b}" # Web Apps by 123apps
-        ]
-        );
+      ExtensionSettings = {
+        # "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+      }
+      // L2A makeExtNav [
+        "easyscreenshot@mozillaonline.com" # Easy Screenshot
+        "simple-tab-groups@drive4ik" # Simple Tab Groups
+        "@testpilot-containers" # Firefox Multi-Account Containers
+        "keepassxc-browser@keepassxc.org" # KeePassXC-Browser
+        "addon@darkreader.org" # Dark Reader
+        # "zotero@chnm.gmu.edu" # Zotero Connector (unavaillable at addons.mozilla.org)
+        "button@scholar.google.com" # Google Scholar Button
+        "{6031c27b-5ae2-4449-a7fd-ac7feabb4ef3}" # Sci-Hub
+        "{0e10f3d7-07f6-4f12-97b9-9b27e07139a5}" # Netcraft Extension
+      ]
+      // L2A makeExt [
+        "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}" # Auto Tab Discard
+        "jid1-BoFifL9Vbdl2zQ@jetpack" # Decentraleyes
+        "firefox.container-shortcuts@strategery.io" # Easy Container Shortcuts
+        "{1018e4d6-728f-4b20-ad56-37578a4de76b}" # Flagfox
+        "jid1-AQqSMBYb0a8ADg@jetpack" # Mailvelope
+        "jid1-MnnxcxisBPnSXQ@jetpack" # Privacy Badger
+        "{2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c}" # Search by Image
+        "uBlock0@raymondhill.net" # uBlock Origin
+        "{b9db16a4-6edc-47ec-a1f4-b86292ed211d}" # Video DownloadHelper
+        "rto@rto.rto" # РуТрекер - официальный плагин (доступ и пр.)
+        "@amiunique-extension" # AmIUnique
+        "{96ef5869-e3ba-4d21-b86e-21b163096400}" # Font Fingerprint Defender
+        # "queryamoid@kaply.com" # Query AMO Addon ID
+        "{a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7}" # User-Agent Switcher and Manager
+        # "{2cf5dbed-78fe-4bd5-9524-38fdf837be98}"  # WebGL Fingerprint Defender
+        # "{55f61747-c3d3-4425-97f9-dfc19a0be23c}"  # Spoof Timezone
+        # "CanvasBlocker@kkapsner.de"  # CanvasBlocker  # Breaks certain websites
+        # "2.0@disconnect.me"  # Disconnect  # Breaks certain websites
+        # "jid1-ZAdIEUB7XOzOJw@jetpack"  # DuckDuckGo Privacy Essentials  # Breaks certain websites
+        # "{73a6fe31-595d-460b-a920-fcc0f8843232}" # NoScript
+        # "suziwen1@gmail.com" # Proxy SwitchyOmega 3 (ZeroOmega)
+        # "{cb08faed-9460-474a-ba0b-d98b13b5e001}" # Regex Search
+        # "{e662576a-2f73-4069-bcca-ddf440fea62b}" # Web Apps by 123apps
+      ];
       HardwareAcceleration = true;
       HttpsOnlyMode = "force_enabled";
-      OfferToSaveLoginsDefault = false;
       OfferToSaveLogins = false;
+      # OfferToSaveLoginsDefault = false;  # Errors is non-default is present
       PasswordManagerEnabled = false;
       PostQuantumKeyAgreementEnabled = true;
       Permissions = {
@@ -161,6 +188,12 @@
           Locked = true;
         };
       };
+      Homepage = {
+        URL = "about:newtab";
+        Locked = true;
+        StartPage = "previous-session";
+      };
+      ManualAppUpdateOnly = true;
       PrimaryPassword = true;
       # Proxy = {
       #   Mode = "manual";
@@ -195,6 +228,15 @@
       };
       NetworkPrediction = false;
       NewTabPage = true;
+      PDFjs = {
+        Enabled = false;
+      };
+      Permissions = {
+        Notifications = {
+          BlockNewRequests = true;
+          Locked = true;
+        };
+      };
       Preferences = {
         "browser.contentblocking.category" = {
           Value = "strict";
@@ -208,9 +250,14 @@
           Value = true;
           Status = "locked";
         };
-        "network.proxy.socks_remote_dns" = {  # for flagfox
+        "network.proxy.socks_remote_dns" = {
+          # for flagfox
           Value = true;
           Status = "user";
+        };
+        "security.csp.reporting.enabled" = {
+          Value = false;
+          Status = "locked";
         };
       };
     };
