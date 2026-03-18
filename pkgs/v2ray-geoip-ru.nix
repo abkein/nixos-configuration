f200faae21c66895ed2816a029549f583d235ac3
abkein <rickbatra0z@gmail.com>
2026-03-18 17:02:01 +0300

    Added packages for Russia specific v2ray rules

---
+++ pkgs/v2ray-geoip-ru.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  fetchurl,
+  stdenvNoCC,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "russia-v2ray-geoip";
+  version = "202603181028";
+
+  src = fetchurl {
+    url = "https://github.com/runetfreedom/russia-v2ray-rules-dat/releases/download/${finalAttrs.version}/geoip.dat";
+    hash = "sha256-vuzzt5LTU35uEOsZDkZPJpa7pyLFUE0QH49cnFjF3ZY=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/v2ray/
+    install --mode=444 geoip.dat "$out/share/v2ray/geoip.dat"
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Russia specific GeoIP for V2Ray";
+    homepage = "https://github.com/runetfreedom/russia-v2ray-rules-dat";
+    license = lib.licenses.cc-by-sa-40;
+    maintainers = with lib.maintainers; [ abkein ];
+  };
+})
---
+++ pkgs/v2ray-geosite-ru.nix
@@ -0,0 +1,30 @@
+{
+  lib,
+  fetchurl,
+  stdenvNoCC,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "russia-v2ray-geosite";
+  version = "202603181028";
+
+  src = fetchurl {
+    url = "https://github.com/runetfreedom/russia-v2ray-rules-dat/releases/download/${finalAttrs.version}/gesite.dat";
+    hash = "sha256-lJAw2SdINlIfY5rDgPannzqYme+hbwvMDwpl8AFKDE4=";
+  };
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/v2ray/
+    install --mode=444 geosite.dat "$out/share/v2ray/geosite.dat"
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Russia specific GeoSite data for V2Ray";
+    homepage = "https://github.com/runetfreedom/russia-v2ray-rules-dat";
+    license = lib.licenses.cc-by-sa-40;
+    maintainers = with lib.maintainers; [ abkein ];
+  };
+})
