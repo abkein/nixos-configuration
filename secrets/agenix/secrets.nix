a768ddaf5795eac2d9169481dc8bd73649eb2420
abkein <rickbatra0z@gmail.com>
2026-05-04 19:35:22 +0300

    Rekey secrets

--- secrets/agenix/secrets.nix
+++ secrets/agenix/secrets.nix
@@ -1,23 +1,28 @@
 let
   kein = "age1yubikey1q02n4jegke9g9ntr7kcymns0pjpyqfllnr4k5tesa79az7mau8s2gfsnx92";
-  # kein = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzMEb6MSQOJnLdf3EdsrsPuiRYJ3Weg00/HbJ+3JeVv";
-  users = [ kein ];
-
-  # system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPJDyIr/FSz1cJdcoW69R+NrWzwGK/+3gJpqD1t8L2zE";
-  # system2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzxQgondgEYcLpcPdJLrTdNgZ2gznOHCAxMdaceTUT1";
-  # systems = [ system1 system2 ];
+  home = "age1349d9ags7k4r9tpvzmtmrc4yrt3q2jd6myp2e0cg06tqt87qjp5qzc0nqe";
+  root = "age1tn9ljj02mvx0mfkw9fzlv8pjpvqpsv72f0darwytuq0ga2x4m5dq8vxk63";
 in
 {
   "encrypted/nix-access-tokens.conf.age" = {
-    publicKeys = [ kein ];
+    publicKeys = [
+      kein
+      root
+    ];
     armor = true;
   };
   "encrypted/nix-netrc.age" = {
-    publicKeys = [ kein ];
+    publicKeys = [
+      kein
+      root
+    ];
     armor = true;
   };
   "encrypted/syncthingPass.age" = {
-    publicKeys = [ kein ];
+    publicKeys = [
+      kein
+      home
+    ];
     armor = true;
   };
 }
