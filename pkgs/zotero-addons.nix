dbd5b92ba4177c2ab128daf9f9cf9575296fa1a2
abkein <rickbatra0z@gmail.com>
2026-05-15 19:54:29 +0300

    Zotero-addons

--- pkgs/zotero-addons.nix
+++ pkgs/zotero-addons.nix
@@ -1,24 +1,24 @@
 pkgs:
 let
-  inherit (pkgs) lib callPackage makeOverridable;
+  inherit (pkgs) lib makeOverridable;
 
   buildZoteroXpiAddon = makeOverridable (
-    { stdenv ? pkgs.stdenv
-    , fetchurl ? pkgs.fetchurl
-    , pname
-    , version
-    , addonId
-    , url
-    , hash
-    , meta
-    , ...
+    {
+      stdenv ? pkgs.stdenv,
+      # fetchurl ? pkgs.fetchurl,
+      pname,
+      version,
+      addonId,
+      # url,
+      # hash,
+      src,
+      meta,
+      ...
     }:
     stdenv.mkDerivation {
       name = "${pname}-${version}";
 
-      inherit meta;
-
-      src = fetchurl { inherit url hash; };
+      inherit src meta;
 
       preferLocalBuild = true;
       allowSubstitutes = true;
@@ -30,30 +30,165 @@ let
       '';
     }
   );
+  fetchGitHubReleaseFile =
+    {
+      owner,
+      repo,
+      tag,
+      file,
+      hash,
+    }:
+    pkgs.fetchurl {
+      inherit hash;
+      url = "https://github.com/${owner}/${repo}/releases/download/${tag}/${file}";
+    };
 in
 {
-  zotero-open-pdf = buildZoteroXpiAddon rec {
-    pname = "zotero-open-pdf";
-    version = "0.0.11";
-    addonId = "open-pdf@iris-advies.com";
+  format-metadata = buildZoteroXpiAddon rec {
+    pname = "zotero-format-metadata";
+    version = "3.1.0";
+    addonId = "zotero-format-metadata@northword.cn";
 
-    url = "https://github.com/retorquere/zotero-open-pdf/releases/download/v${version}/zotero-open-pdf-${version}.xpi";
-    hash = "sha256-j6S7SQncj99+XrLN76EjnaEZqrjZZwOWjVtaDV1kKYI=";
+    src = fetchGitHubReleaseFile {
+      owner = "northword";
+      repo = pname;
+      tag = "v${version}";
+      file = "linter-for-zotero.xpi";
+      hash = "sha256-o/Afn9u4gTK4T9VFJVk9uRhpdCWUSK+r7eCZgsF2vG0=";
+    };
 
     meta = with lib; {
-      homepage = "https://github.com/retorquere/zotero-open-pdf";
-      # license unclear
+      homepage = "https://github.com/northword/zotero-format-metadata";
+      license = licenses.agpl3Only;
+      platforms = platforms.all;
+
+      # Does stdenv support these?
+      # description = "Python wrapper for the arXiv API";
+      # changelog = "https://github.com/lukasschwab/arxiv.py/releases/tag/${src.tag}";
+    };
+  };
+
+  zutilo = buildZoteroXpiAddon rec {
+    pname = "Zutilo";
+    version = "4.2.1";
+    addonId = "zutilo@www.wesailatdawn.com";
+
+    src = fetchGitHubReleaseFile {
+      owner = "wshanks";
+      repo = pname;
+      tag = "v${version}";
+      file = "zutilo.xpi";
+      hash = "sha256-i/sKIdw87V1rh6jfkjyE+XgL3JQjS4NCmXPay17yCho=";
+    };
+
+    meta = with lib; {
+      homepage = "https://github.com/wshanks/Zutilo";
+      license = licenses.agpl3Only;
+      platforms = platforms.all;
+    };
+  };
+
+  inspire = buildZoteroXpiAddon rec {
+    pname = "zotero-inspire";
+    version = "3.0.2";
+    addonId = "zoteroinspire@itp.ac.cn";
+
+    src = fetchGitHubReleaseFile {
+      owner = "fkguo";
+      repo = pname;
+      tag = "v${version}";
+      file = "zotero-inspire.xpi";
+      hash = "sha256-ITrrZrDIri1qEfyNhobwmOW207bloFipKsrWFzqA8UU=";
+    };
+
+    meta = with lib; {
+      homepage = "https://github.com/fkguo/zotero-inspire";
+      license = licenses.mpl20;
+      platforms = platforms.all;
+    };
+  };
+
+  short-doi = buildZoteroXpiAddon rec {
+    pname = "zotero-shortdoi";
+    version = "1.6.0"; # "1.5.0";
+    addonId = "zoteroshortdoi@wiernik.org";
+
+    # src = fetchGitHubReleaseFile {
+    #   owner = "bwiernik";
+    #   repo = pname;
+    #   tag = "v${version}";
+    #   file = "zotero-doi-manager-${version}.xpi";
+    #   hash = "sha256-dEruz3MDkyp3Gbcn37Z8/oJpsK9IqtS+sE7LWXeuwxY=";
+    # };
+
+    # Temporarily? fetch from fork
+    src = fetchGitHubReleaseFile {
+      owner = "JuliusBairaktaris";
+      repo = pname;
+      tag = "v${version}";
+      file = "zotero-doi-manager-${version}.xpi";
+      hash = "sha256-pSqEbRsoAYT2M7/mAwV0CUs9bSwMxE2tmKNxEdoJ3qA=";
+    };
+
+    meta = with lib; {
+      homepage = "https://github.com/bwiernik/zotero-shortdoi";
+      license = licenses.mpl20;
+      platforms = platforms.all;
+    };
+  };
+
+  pmcid-fetcher = buildZoteroXpiAddon rec {
+    pname = "zotero-pmcid-fetcher";
+    version = "1.0.2";
+    addonId = "zotero-pmcid-fetcher@iris-advies.com";
+
+    src = fetchGitHubReleaseFile {
+      owner = "retorquere";
+      repo = pname;
+      tag = "v${version}";
+      file = "zotero-pmcid-fetcher-${version}.xpi";
+      hash = "sha256-X8v2/NV6+IctVhyYcqNDR6Yj2RE7nl8ERYazUIlls78=";
+    };
+
+    meta = with lib; {
+      homepage = "https://github.com/retorquere/zotero-pmcid-fetcher";
+      # unclear license
+      platforms = platforms.all;
+    };
+  };
+
+  better-bibtex = buildZoteroXpiAddon rec {
+    pname = "zotero-better-bibtex";
+    version = "9.0.23";
+    addonId = "better-bibtex@iris-advies.com";
+
+    src = fetchGitHubReleaseFile {
+      owner = "retorquere";
+      repo = pname;
+      tag = "v${version}";
+      file = "zotero-better-bibtex-${version}.xpi";
+      hash = "sha256-CX6gwKn/1kyJQpWtN1LcBqmz8HuIBqhK7fnW/PQ1cXw=";
+    };
+
+    meta = with lib; {
+      homepage = "https://github.com/retorquere/zotero-better-bibtex";
+      license = [ licenses.mit ];
       platforms = platforms.all;
     };
   };
 
   cita = buildZoteroXpiAddon rec {
-    pname = "cita";
-    version = "1.0.0-beta.7";
+    pname = "zotero-cita";
+    version = "0.5.5";
     addonId = "zotero-wikicite@wikidata.org";
 
-    url = "https://github.com/diegodlh/zotero-cita/releases/download/v${version}/zotero-cita.xpi";
-    hash = "sha256-/RcO+W1LXp938L7kM6Yues8YvyrihYI/UgyRB66Rn9Y=";
+    src = fetchGitHubReleaseFile {
+      owner = "diegodlh";
+      repo = pname;
+      tag = "v${version}";
+      file = "zotero-cita-v${version}.xpi";
+      hash = "sha256-8L7wUABOMXoQ+irGZtcgpZu8cXHYLFR7UgCGKRmlJmQ=";
+    };
 
     meta = with lib; {
       homepage = "https://github.com/diegodlh/zotero-cita";
@@ -62,104 +197,142 @@ in
     };
   };
 
-  zotero-storage-scanner = buildZoteroXpiAddon rec {
-    pname = "zotero-storage-scanner";
-    version = "5.0.12";
-    addonId = "storage-scanner@iris-advies.com";
+  open-pdf = buildZoteroXpiAddon rec {
+    pname = "zotero-open-pdf";
+    version = "1.0.12";
+    addonId = "open-pdf@iris-advies.com";
 
-    url = "https://github.com/retorquere/zotero-storage-scanner/releases/download/v${version}/zotero-storage-scanner-${version}.xpi";
-    hash = "sha256-WGF3//sdZ8qk9IKOrgP7kbi7Yz5iRMXCbr2wQeXqpT8=";
+    src = fetchGitHubReleaseFile {
+      owner = "retorquere";
+      repo = pname;
+      tag = "v${version}";
+      file = "zotero-open-pdf-${version}.xpi";
+      hash = "sha256-sqjFViYbMXxwoJOqAi0vA7Ewuf4z1XjtT3a8aP9qu38=";
+    };
 
     meta = with lib; {
-      homepage = "https://github.com/retorquere/zotero-storage-scanner";
+      homepage = "https://github.com/retorquere/zotero-open-pdf";
       # license unclear
       platforms = platforms.all;
     };
   };
 
-  zotero-auto-index = buildZoteroXpiAddon rec {
-    pname = "zotero-auto-index";
-    version = "5.0.9";
-    addonId = "auto-index@iris-advies.com";
+  attachment-scanner = buildZoteroXpiAddon rec {
+    pname = "zotero-attachment-scanner";
+    version = "0.4.1";
+    addonId = "attachmentscanner@changlab.um.edu.mo";
 
-    url = "https://github.com/retorquere/zotero-auto-index/releases/download/v${version}/zotero-auto-index-${version}.xpi";
-    hash = "sha256-VmOZn+6g0KLCxkLafc+5DaTP9/Fvx32a9LUBD6NQ8MI=";
+    src = fetchGitHubReleaseFile {
+      owner = "SciImage";
+      repo = pname;
+      tag = "v${version}";
+      file = "attachmentscanner-${version}.xpi";
+      hash = "sha256-N7Vl+I2tMumF0wtNJUkspQpiwvOBR/eK0W2Kpcb74CI=";
+    };
 
     meta = with lib; {
-      homepage = "https://github.com/retorquere/zotero-auto-index";
-      # TODO license
+      homepage = "https://github.com/SciImage/zotero-attachment-scanner";
+      license = licenses.mit;
       platforms = platforms.all;
     };
   };
+
+  # Archived, only up to Zotero 5
+  # zotero-auto-index = buildZoteroXpiAddon rec {
+  #   pname = "zotero-auto-index";
+  #   version = "5.0.9";
+  #   addonId = "auto-index@iris-advies.com";
+
+  #   url = "https://github.com/retorquere/zotero-auto-index/releases/download/v${version}/zotero-auto-index-${version}.xpi";
+  #   hash = "sha256-VmOZn+6g0KLCxkLafc+5DaTP9/Fvx32a9LUBD6NQ8MI=";
+
+  #   meta = with lib; {
+  #     homepage = "https://github.com/retorquere/zotero-auto-index";
+  #     # TODO license
+  #     platforms = platforms.all;
+  #   };
+  # };
 
   zotero-ocr = buildZoteroXpiAddon rec {
     pname = "zotero-ocr";
-    version = "0.8.1";
+    version = "0.9.5.1";
     addonId = "zotero-ocr@bib.uni-mannheim.de";
 
-    url = "https://github.com/UB-Mannheim/zotero-ocr/releases/download/${version}/zotero-ocr-${version}.xpi";
-    hash = "sha256-NFVWvDdToOTGU+y1AeAKMg2vukmGKcTZ3CDhvYEEopM=";
+    src = fetchGitHubReleaseFile {
+      owner = "UB-Mannheim";
+      repo = pname;
+      tag = "${version}";
+      file = "zotero-ocr-${version}.xpi";
+      hash = "sha256-8QCz5gy087tpFmV261fnb3ACsk/1JkjCpoJ9aLU1r44=";
+    };
 
     meta = with lib; {
       homepage = "https://github.com/UB-Mannheim/zotero-ocr";
-      # TODO license
+      license = licenses.agpl3Only;
       platforms = platforms.all;
     };
   };
 
-  zotero-robustlinks = buildZoteroXpiAddon rec {
-    pname = "zotero-robustlinks";
-    version = "2.0.0-20220320145937";
-    addonId = "zotero-robustlinks@mementoweb.org";
+  # Only up to Zotero 6
+  # zotero-robustlinks = buildZoteroXpiAddon rec {
+  #   pname = "zotero-robustlinks";
+  #   version = "2.0.0-20220320145937";
+  #   addonId = "zotero-robustlinks@mementoweb.org";
 
-    url = "https://github.com/lanl/Zotero-Robust-Links-Extension/releases/download/v${version}/robustlinks.xpi";
-    hash = "sha256-U4ZPhFP06YP8xXmx8p0lTUa0nDtZN3YyrCPxtgz7D0E=";
+  #   url = "https://github.com/lanl/Zotero-Robust-Links-Extension/releases/download/v${version}/robustlinks.xpi";
+  #   hash = "sha256-U4ZPhFP06YP8xXmx8p0lTUa0nDtZN3YyrCPxtgz7D0E=";
 
-    meta = with lib; {
-      homepage = "https://robustlinks.mementoweb.org/zotero/";
-      # TODO license
-      platforms = platforms.all;
-    };
-  };
+  #   meta = with lib; {
+  #     homepage = "https://robustlinks.mementoweb.org/zotero/";
+  #     # TODO license
+  #     platforms = platforms.all;
+  #   };
+  # };
 
-  zotero-abstract-cleaner = buildZoteroXpiAddon rec {
-    pname = "zotero-abstract-cleaner";
-    version = "0.1.6";
-    addonId = "ZoteroAbstractCleaner@carter-tod.com";
+  # Discontinued
+  # zotero-abstract-cleaner = buildZoteroXpiAddon rec {
+  #   pname = "zotero-abstract-cleaner";
+  #   version = "0.1.6";
+  #   addonId = "ZoteroAbstractCleaner@carter-tod.com";
 
-    url = "https://github.com/dcartertod/zotero-plugins/releases/download/${version}/ZoteroAbstractCleaner.xpi";
-    hash = "sha256-6ankwlieLLHiUPwhXptWwyomUaKCwEbVebTOWSbrLWs=";
+  #   url = "https://github.com/dcartertod/zotero-plugins/releases/download/${version}/ZoteroAbstractCleaner.xpi";
+  #   hash = "sha256-6ankwlieLLHiUPwhXptWwyomUaKCwEbVebTOWSbrLWs=";
 
-    meta = with lib; {
-      homepage = "https://github.com/dcartertod/zotero-plugins/tree/main/ZoteroAbstractCleaner";
-      # TODO license
-      platforms = platforms.all;
-    };
-  };
+  #   meta = with lib; {
+  #     homepage = "https://github.com/dcartertod/zotero-plugins/tree/main/ZoteroAbstractCleaner";
+  #     license = licenses.asl20;
+  #     platforms = platforms.all;
+  #   };
+  # };
 
-  zotero-preview = buildZoteroXpiAddon rec {
-    pname = "zotero-preview";
-    version = "0.7.0";
-    addonId = "zoteropreview@carter-tod.com";
+  # Discontinued, up to Zotero 7
+  # zotero-preview = buildZoteroXpiAddon rec {
+  #   pname = "zotero-preview";
+  #   version = "0.7.0";
+  #   addonId = "zoteropreview@carter-tod.com";
 
-    url = "https://github.com/dcartertod/zotero-plugins/releases/download/${version}/ZoteroPreview7.xpi";
-    hash = "sha256-tLZIFNhhkMa0tO7HRJIiuEQnpLrNwLD1e96Wl/qXv4g=";
+  #   url = "https://github.com/dcartertod/zotero-plugins/releases/download/${version}/ZoteroPreview7.xpi";
+  #   hash = "sha256-tLZIFNhhkMa0tO7HRJIiuEQnpLrNwLD1e96Wl/qXv4g=";
 
-    meta = with lib; {
-      homepage = "https://github.com/dcartertod/zotero-plugins/tree/main/ZoteroPreview";
-      # TODO license
-      platforms = platforms.all;
-    };
-  };
+  #   meta = with lib; {
+  #     homepage = "https://github.com/dcartertod/zotero-plugins/tree/main/ZoteroPreview";
+  #     license = licenses.asl20;
+  #     platforms = platforms.all;
+  #   };
+  # };
 
   zotmoov = buildZoteroXpiAddon rec {
     pname = "zotmoov";
-    version = "1.1.14";
+    version = "1.2.29";
     addonId = "zotmoov@wileyy.com";
 
-    # TODO Remove "Beta" when the release is stable
-    url = "https://github.com/wileyyugioh/zotmoov/releases/download/${version}/zotmoov-${version}-fx.xpi";
-    hash = "sha256-Csp+cX7YouO8u7XZoY7gNnU5Z8V9dDe7+hxFKOqej3Q=";
+    src = fetchGitHubReleaseFile {
+      owner = "wileyyugioh";
+      repo = pname;
+      tag = "${version}";
+      file = "zotmoov-${version}-fx.xpi";
+      hash = "sha256-DDjziK93E6F4nxEXxLVA/bVj0OsS0fnuUd0nZR1yFl0=";
+    };
 
     meta = with lib; {
       homepage = "https://github.com/wileyyugioh/zotmoov";
@@ -168,13 +341,18 @@ in
     };
   };
 
-  zotero-delitemwithatt = buildZoteroXpiAddon rec {
-    pname = "zotero-delitemwithatt";
-    version = "0.3.3";
+  delitemwithatt = buildZoteroXpiAddon rec {
+    pname = "delitemwithatt";
+    version = "0.4.5";
     addonId = "delitemwithatt@redleaf.me";
 
-    url = "https://github.com/redleafnew/delitemwithatt/releases/download/${version}/delitemwithatt.xpi";
-    hash = "sha256-rUJrDIT1YaFXRYhqWegZXdYUHG9toQ6mbpFlVw1qM5w=";
+    src = fetchGitHubReleaseFile {
+      owner = "redleafnew";
+      repo = pname;
+      tag = "v${version}";
+      file = "del-item-with-attachment.xpi";
+      hash = "sha256-XCbwegs8jZ0h/KNaKnbF8D6Jj1iRLNbqasHKxVlZtnY=";
+    };
 
     meta = with lib; {
       homepage = "https://github.com/redleafnew/delitemwithatt";
@@ -184,12 +362,17 @@ in
   };
 
   scite-zotero = buildZoteroXpiAddon rec {
-    pname = "scite-zotero";
-    version = "2.0.2";
+    pname = "scite-zotero-plugin";
+    version = "2.0.5";
     addonId = "scite-zotero-plugin@scite.ai";
 
-    url = "https://github.com/scitedotai/scite-zotero-plugin/releases/download/v${version}/scite-zotero-plugin-${version}.xpi";
-    hash = "sha256-Etb8K9MrkzBGnQYDQeoexkV13VPPemIYNW8ubyrLlhc=";
+    src = fetchGitHubReleaseFile {
+      owner = "scitedotai";
+      repo = pname;
+      tag = "v${version}";
+      file = "scite-zotero-plugin-${version}.xpi";
+      hash = "sha256-Ws8vMjHMabMUOkY+OlvY/pPIEwh9hycg7GZQUn2YT7M=";
+    };
 
     meta = with lib; {
       homepage = "https://github.com/scitedotai/scite-zotero-plugin";
@@ -198,14 +381,18 @@ in
     };
   };
 
-
   zotero-gpt = buildZoteroXpiAddon rec {
     pname = "zotero-gpt";
-    version = "1.2.3";
+    version = "3.1.4";
     addonId = "zoterogpt@polygon.org";
 
-    url = "https://github.com/MuiseDestiny/zotero-gpt/releases/download/${version}/zotero-gpt.xpi";
-    hash = "sha256-qxl0/u3JJx1moXK7PcixTKeNNnJywwtkwgWYx5GHphM=";
+    src = fetchGitHubReleaseFile {
+      owner = "MuiseDestiny";
+      repo = pname;
+      tag = "${version}";
+      file = "zotero-gpt.xpi";
+      hash = "sha256-NPB3oJtp0+e5NVp+isXFLuE/GPqewkxdYFxn/wKnfl0=";
+    };
 
     meta = with lib; {
       homepage = "https://github.com/MuiseDestiny/zotero-gpt";
@@ -219,8 +406,13 @@ in
     version = "2.0.18";
     addonId = "Knowledge4Zotero@windingwind.com";
 
-    url = "https://github.com/windingwind/zotero-better-notes/releases/download/v${version}/better-notes-for-zotero.xpi";
-    hash = "sha256-02BMrJKyeQUJ9eafB8cDiu6ks4V+tcJecXt7Zv7F+Us=";
+    src = fetchGitHubReleaseFile {
+      owner = "windingwind";
+      repo = pname;
+      tag = "v${version}";
+      file = "better-notes-for-zotero.xpi";
+      hash = "sha256-452gSKxqRC7jvEtRL6mrePXVROfyXwQfKsht72MFC1s=";
+    };
 
     meta = with lib; {
       homepage = "https://github.com/windingwind/zotero-better-notes";
@@ -231,11 +423,16 @@ in
 
   ai-research-assistant = buildZoteroXpiAddon rec {
     pname = "ai-research-assistant";
-    version = "0.7.2";
+    version = "0.7.5";
     addonId = "aria@apex973.com";
 
-    url = "https://github.com/lifan0127/ai-research-assistant/releases/download/v${version}/aria.xpi";
-    hash = "sha256-ryZUt7gnAUayOCccuqwXp3VIMQYkY9gM+obQrmL2NPQ=";
+    src = fetchGitHubReleaseFile {
+      owner = "lifan0127";
+      repo = pname;
+      tag = "v${version}";
+      file = "aria.xpi";
+      hash = "sha256-xqki7703PPYdHqUwDGyG3+nBfabYVfnGz10EJ2oeH7c=";
+    };
 
     meta = with lib; {
       homepage = "https://github.com/lifan0127/ai-research-assistant";
