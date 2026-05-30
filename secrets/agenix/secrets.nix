5ea8b545186a519eb4369947a555b17480e69e58
abkein <rickbatra0z@gmail.com>
2026-05-30 04:37:23 +0300

    Rekey

--- secrets/agenix/secrets.nix
+++ secrets/agenix/secrets.nix
@@ -1,20 +1,23 @@
 let
   kein = "age1yubikey1q02n4jegke9g9ntr7kcymns0pjpyqfllnr4k5tesa79az7mau8s2gfsnx92";
   home = "age1349d9ags7k4r9tpvzmtmrc4yrt3q2jd6myp2e0cg06tqt87qjp5qzc0nqe";
-  root = "age1tn9ljj02mvx0mfkw9fzlv8pjpvqpsv72f0darwytuq0ga2x4m5dq8vxk63";
+  jeta = "age1tn9ljj02mvx0mfkw9fzlv8pjpvqpsv72f0darwytuq0ga2x4m5dq8vxk63";
+  yun = "age17aearyq69n484l270x0hh8z7uczpyz57v972xfrwrtcayyfyvaqsrd3s66";
 in
 {
   "encrypted/nix-access-tokens.conf.age" = {
     publicKeys = [
       kein
-      root
+      jeta
+      yun
     ];
     armor = true;
   };
   "encrypted/nix-netrc.age" = {
     publicKeys = [
       kein
-      root
+      jeta
+      yun
     ];
     armor = true;
   };
@@ -22,6 +25,7 @@ in
     publicKeys = [
       kein
       home
+      jeta
     ];
     armor = true;
   };
