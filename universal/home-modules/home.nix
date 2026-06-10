b1142f78710edbd9b2764024669b9e2ecf317e21
abkein <rickbatra0z@gmail.com>
2026-06-10 20:08:23 +0300

    Universal: move xdg to home.nix from shell.nix

--- universal/home-modules/home.nix
+++ universal/home-modules/home.nix
@@ -12,4 +12,15 @@
       enableZshIntegration = true;
     };
   };
+
+  xdg = {
+    enable = true;
+    # defaults:
+    # cacheHome = "~/.cache";  # $XDG_CACHE_HOME
+    # configHome = "~/.config";  # $XDG_CONFIG_HOME
+    # dataHome = "~/.local/share";  # $XDG_DATA_HOME
+    # stateHome = "~/.local/state";  # $XDG_STATE_HOME
+    # binHome = "~/.local/bin"; # $XDG_BIN_HOME
+    localBinInPath = true;
+  };
 }
--- universal/home-modules/shell.nix
+++ universal/home-modules/shell.nix
@@ -1,16 +1,5 @@
 { config, pkgs, ... }:
 {
-  xdg = {
-    enable = true;
-    # defaults:
-    # cacheHome = "~/.cache";  # $XDG_CACHE_HOME
-    # configHome = "~/.config";  # $XDG_CONFIG_HOME
-    # dataHome = "~/.local/share";  # $XDG_DATA_HOME
-    # stateHome = "~/.local/state";  # $XDG_STATE_HOME
-    # binHome = "~/.local/bin"; # $XDG_BIN_HOME
-    localBinInPath = true;
-  };
-
   programs = {
     ripgrep.enable = true;
     ripgrep-all.enable = true;
