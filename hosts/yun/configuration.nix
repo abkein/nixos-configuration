ece88abac8226bcf49d0adac9506f9b3ee0e6d55
abkein <rickbatra0z@gmail.com>
2026-06-10 21:25:18 +0300

    Universal: follow self-declared convention

---
+++ hosts/yun/configuration.nix
@@ -0,0 +1,42 @@
+{ cfg, ... }:
+{
+  imports = [
+    ../../universal/system-modules/user.nix
+    ../../universal/system-modules/core.nix
+    ../../universal/system-modules/zram.nix
+    ./hardware-configuration.nix
+    ./system-modules
+  ];
+
+  age = {
+    identityPaths = [ "/root/keys/actual_age_root.key" ];
+    secrets = {
+      "nix-access-tokens.conf" = {
+        file = ../../${cfg.secrets}/nix-access-tokens.conf.age;
+      };
+      "nix-netrc" = {
+        file = ../../${cfg.secrets}/nix-netrc.age;
+      };
+    };
+  };
+
+  time.timeZone = "Europe/Helsinki";
+
+  # environment.systemPackages = with pkgs; [
+  # ];
+
+  users.users = {
+    ${cfg.username} = {
+      hashedPassword = "$y$j9T$g95Qjm6uFhDpwt19Mc81D0$Et9NONGjndR21I5qopLLE2X/dqs6Ut4Hxw/VzI6GU24";
+      # packages = with pkgs; [ ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJF5/M+hRBcahbnuGK+iHB0obByeYzJxsKKRHpO7gxXP"
+      ];
+    };
+  };
+
+  # programs = {
+  # };
+
+  system.stateVersion = "25.11";
+}
