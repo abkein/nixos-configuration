bbb51451d1c7f0ec8fb36f5e699f48ac2fdc2a87
abkein <rickbatra0z@gmail.com>
2026-06-11 08:18:54 +0300

    Jeta: add plugdev group

--- hosts/jeta/configuration.nix
+++ hosts/jeta/configuration.nix
@@ -146,8 +146,8 @@
     };
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
+    groups.plugdev = { };
     users = {
       "${cfg.username}" = {
         extraGroups = [
