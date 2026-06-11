c0aa19561e8d47432b32821919798bdfa14a9048
abkein <rickbatra0z@gmail.com>
2026-06-11 07:11:54 +0300

    Jeta: cleanup

--- hosts/jeta/configuration.nix
+++ hosts/jeta/configuration.nix
@@ -79,49 +79,7 @@
         workstation = true;
       };
     };
-    tlp = {
-      enable = true;
-      settings = {
-        TLP_AUTO_SWITCH = 1;
-        TLP_DEFAULT_MODE = "BAL";
-
-        RADEON_DPM_PERF_LEVEL_ON_AC = "auto";
-        RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
-        RADEON_DPM_PERF_LEVEL_ON_SAV = "low";
-
-        CPU_DRIVER_OPMODE_ON_AC = "active";
-        CPU_DRIVER_OPMODE_ON_BAT = "active";
-        CPU_DRIVER_OPMODE_ON_SAV = "active";
-
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_SCALING_GOVERNOR_ON_SAV = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-        CPU_ENERGY_PERF_POLICY_ON_SAV = "power";
-
-        DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth nfc wifi wwan";
-
-        DEVICES_TO_DISABLE_ON_LAN_CONNECT = "wifi wwan";
-        DEVICES_TO_DISABLE_ON_WIFI_CONNECT = "wwan";
-        DEVICES_TO_DISABLE_ON_WWAN_CONNECT = "wifi";
-
-        PCIE_ASPM_ON_AC = "default";
-        PCIE_ASPM_ON_BAT = "powersave";
-
-      };
-      pd.enable = true;
-    };
-    # acpid = {
-    #   # Register commands for events, e.g.
-    #   # "button/power.*" "button/lid.*" "ac_adapter.*" "button/mute.*" "button/volumedown.*" "cd/play.*" "cd/next.*"
-    #   enable = true;
-    #   # logEvents = true;
-    # };
     blueman.enable = true;
-    upower.enable = true;
-    thermald.enable = true;
     udev = {
       enable = true;
       packages = with pkgs; [
@@ -129,7 +87,7 @@
         # onlykey-cli  # insecure because of ecdsa package
         sane-airscan
       ];
-    };
+    }; # hardware.glasgow — plugdev
     pcscd = {
       enable = true;
       # plugins = [ pkgs.ccid ];
@@ -374,24 +332,12 @@
       powerOnBoot = false;
       settings = {
         General = {
-          # Specify the policy to the JUST-WORKS repairing initiated by peer
-          # Possible values: "never", "confirm", "always"
-          # Defaults to "never"
           JustWorksRepairing = "confirm";
-          # Shows battery charge of connected devices on supported
-          # Bluetooth adapters.
-          # Enables D-Bus experimental interfaces. Defaults to 'false'.
           Experimental = true;
-          # When enabled other devices can connect faster to us, however
-          # the tradeoff is increased power consumption. Defaults to
-          # 'false'.
           FastConnectable = true;
         };
         Policy = {
-          # AutoEnable defines option to enable all controllers when they are found.
-          # This includes adapters present on start as well as adapters that are plugged
-          # in later on. Defaults to 'true'.
-          AutoEnable = true;
+          AutoEnable = false;
         };
       };
     };
--- hosts/jeta/hardware-configuration.nix
+++ hosts/jeta/hardware-configuration.nix
@@ -48,6 +48,54 @@
     kernel.sysctl."kernel.sysrq" = 1;
   };
 
+  services = {
+    upower = {
+      enable = true; # Auto sleep at 5% battery left
+      percentageCritical = 7;
+      percentageAction = 5;
+    };
+    # acpid = {
+    #   # Register commands for events, e.g.
+    #   # "button/power.*" "button/lid.*" "ac_adapter.*" "button/mute.*" "button/volumedown.*" "cd/play.*" "cd/next.*"
+    #   enable = true;
+    #   # logEvents = true;
+    # };
+    tlp = {
+      enable = true;
+      settings = {
+        TLP_AUTO_SWITCH = 1;
+        TLP_DEFAULT_MODE = "BAL";
+
+        RADEON_DPM_PERF_LEVEL_ON_AC = "auto";
+        RADEON_DPM_PERF_LEVEL_ON_BAT = "low";
+        RADEON_DPM_PERF_LEVEL_ON_SAV = "low";
+
+        CPU_DRIVER_OPMODE_ON_AC = "active";
+        CPU_DRIVER_OPMODE_ON_BAT = "active";
+        CPU_DRIVER_OPMODE_ON_SAV = "active";
+
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_SCALING_GOVERNOR_ON_SAV = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        CPU_ENERGY_PERF_POLICY_ON_SAV = "power";
+
+        DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth nfc wifi wwan";
+
+        DEVICES_TO_DISABLE_ON_LAN_CONNECT = "wifi wwan";
+        DEVICES_TO_DISABLE_ON_WIFI_CONNECT = "wwan";
+        DEVICES_TO_DISABLE_ON_WWAN_CONNECT = "wifi";
+
+        PCIE_ASPM_ON_AC = "default";
+        PCIE_ASPM_ON_BAT = "powersave";
+
+      };
+      pd.enable = true;
+    };
+  };
+
   # systemd = {
   #   sleep.extraConfig = ''
   #     HibernateDelaySec=1h
@@ -57,6 +105,10 @@
 
   # hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware = {
+    # amdgpu = {
+    #   initrd.enable = true;
+    #   opencl.enable = true;
+    # };
     # enableAllFirmware = true;
     # enableAllHardware = true;
     enableRedistributableFirmware = true;
