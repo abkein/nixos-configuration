8c02f9d5e6ddacc9c86dc8504e188eede8df41df
abkein <rickbatra0z@gmail.com>
2026-06-13 17:33:59 +0300

    Jeta: disable syncthing

--- hosts/jeta/home-modules/syncthing.nix
+++ hosts/jeta/home-modules/syncthing.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   services.syncthing = {
-    enable = true;
+    enable = false;
     # openDefaultPorts = true;
     guiAddress = "127.0.0.1:8384"; # default
     guiCredentials = {
