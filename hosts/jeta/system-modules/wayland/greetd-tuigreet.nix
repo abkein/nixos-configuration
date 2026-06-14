f77e0f987cadaaeb5e347e918d5de8a8bbe0d4ea
abkein <rickbatra0z@gmail.com>
2026-06-14 04:11:32 +0300

    Jeta: tuigreet works

--- hosts/jeta/system-modules/wayland/greetd-tuigreet.nix
+++ hosts/jeta/system-modules/wayland/greetd-tuigreet.nix
@@ -1,7 +1,25 @@
-{ ... }:
+{ config, pkgs, ... }:
+let
+  sessionData = config.services.displayManager.sessionData;
+  #   log=/var/log/tuigreet/tuigreet.log
+  #   {
+  runTuigreet = pkgs.writeShellScript "run-tuigreet" ''
+    exec ${pkgs.tuigreet}/bin/tuigreet \
+      --time --user-menu       \
+      --debug /var/log/tuigreet/tuigreet-debug.log    \
+      --sessions "${sessionData.desktops}/share/wayland-sessions" \
+      --power-shutdown '${pkgs.systemd}/bin/poweroff' \
+      --power-reboot '${pkgs.systemd}/bin/reboot'
+  '';
+  #    --kb-command 1 --kb-sessions 2 --kb-power 3
+  #'';
+  #   } > "$log" 2>&1
+in
 {
   services.greetd = {
     enable = true;
     useTextGreeter = true;
+    settings.default_session.command = runTuigreet;
   };
+  environment.systemPackages = with pkgs; [ tuigreet ];
 }
