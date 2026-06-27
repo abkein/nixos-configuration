ef5dd38814528901b28dad3f54576103ab72488a
abkein <rickbatra0z@gmail.com>
2026-06-28 00:41:49 +0300

    pkgs: stable ast-serialize

--- pkgs/ast-serialize.nix
+++ pkgs/ast-serialize.nix
@@ -2,11 +2,8 @@
   lib,
   fetchFromGitHub,
   buildPythonPackage,
-
-  # build-system
-  # setuptools,
-  # setuptools-scm,
-  maturin,
+  rustPlatform,
+  pytestCheckHook,
 }:
 buildPythonPackage (finalAttrs: {
   pname = "ast-serialize";
@@ -20,16 +17,21 @@ buildPythonPackage (finalAttrs: {
     sha256 = "sha256-GmhbMraI16J6ePtn7lXAWaJ+9zDH1GdebKIAzm5w9ok=";
   };
 
-  build-system = [
-    maturin
-    # setuptools
-    # setuptools-scm
+  cargoDeps = rustPlatform.fetchCargoVendor {
+    inherit (finalAttrs) pname version src;
+    hash = "sha256-h+BklNeoQaRVWczsE9sFXgvFrnHW5vjWOVaOvLghv0U=";
+  };
+
+  build-system = with rustPlatform; [
+    cargoSetupHook
+    maturinBuildHook
   ];
 
-  # dependencies = [
-  # ];
-
   pythonImportsCheck = [ "ast_serialize" ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
 
   meta = with lib; {
     description = "Python bindings for mypy AST serialization.";
