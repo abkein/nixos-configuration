f715012287b4f4392782b5f92c73e4c695a0445a
abkein <rickbatra0z@gmail.com>
2026-08-17 19:57:08 +0300

    Mathematica pkg

---
+++ pkgs/my-mathematica.nix
@@ -0,0 +1,15 @@
+{ pkgs }:
+pkgs.mathematica.override {
+  versionInfo = {
+    version = "15.0.1";
+    lang = "en";
+    language = "English";
+    hash = "sha256-VzK8CuOhk4sOO5CL4z3rfpY5631F2RN6c0Dh8cExeeg=";
+    installer = "Wolfram_15.0.1.sh";
+  };
+  source = /home/kein/Projects/Mathematica/Wolfram_15.0.1.sh;
+  # pkgs.fetchurl {
+  #   url = "https://example.com/Wolfram_XX.X.X_BNDL_LINUX.sh";
+  #   hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  # };
+}
