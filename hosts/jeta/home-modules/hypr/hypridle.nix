2e8d54c2ff93a9b274104b1bccda6d83162227e9
abkein <rickbatra0z@gmail.com>
2026-09-08 22:00:18 +0300

    jeta: home: hypridle: renew configuration

--- hosts/jeta/home-modules/hypr/hypridle.nix
+++ hosts/jeta/home-modules/hypr/hypridle.nix
@@ -1,18 +1,29 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.hypridle = {
     enable = true;
     settings =
       let
         # FIXME: provide full path to binaries
-        lock_cmd = "pidof hyprlock || hyprlock && hyprctl switchxkblayout at-translated-set-2-keyboard 0 && sleep 1 && grim /home/kein/Pictures/sc.png";
-        suspend_cmd = "if [[ $(cat /sys/class/power_supply/ACAD/online) == '1' && $(cat /sys/class/power_supply/ACAD/type) == 'Mains' ]]; then hyprctl notify 1 5000 0 'fontsize:35 Auto suspending is disabled due to connected power supply.'; else systemctl suspend || loginctl suspend; fi";
+        lock_cmd = "pidof hyprlock || (hyprlock && sleep 1 && hyprctl switchxkblayout at-translated-set-2-keyboard 0)";
+        suspend_condition = pkgs.writeShellScript "hypridle-suspend-condition.sh" ''
+          if [[ $(cat /sys/class/power_supply/ACAD/online) == '1' && $(cat /sys/class/power_supply/ACAD/type) == 'Mains' ]]; then
+            hyprctl notify 1 5000 0 'fontsize:35 Auto suspending is disabled due to connected power supply.'
+            exit 1
+          else
+            exit 0
+          fi
+        '';
         lower_bright = "brightnessctl --device='amdgpu_bl1' -s set 1";
         resume_bright = "brightnessctl --device='amdgpu_bl1' -r";
       in
       {
         general = {
           lock_cmd = lock_cmd;
-          before_sleep_cmd = lock_cmd;
+          before_sleep_cmd = "loginctl lock-sesison";
+          after_sleep_cmd = ''
+            hyprctl dispatch 'hl.dsp.dpms({ action = "enable" })'
+          '';
+          inhibit_sleep = 3;
         };
         listener = [
           {
@@ -22,16 +33,22 @@
           }
           {
             timeout = 120;
-            on-timeout = lock_cmd;
+            on-timeout = "loginctl lock-session";
           }
           {
             timeout = 180;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
+            on-timeout = ''
+              hyprctl dispatch 'hl.dsp.dpms({ action = "disable" })'
+            '';
+            on-resume = ''
+              hyprctl dispatch 'hl.dsp.dpms({ action = "enable" })' && brightnessctl -r
+            '';
           }
           {
             timeout = 7200;
-            on-timeout = suspend_cmd;
+            on-timeout = "systemctl suspend";
+            condition_cmd = "${suspend_condition}";
+            condition_retry = 600;
           }
         ];
       };
