6f8f9e343a9caae36812f40d3df013b75dbd760b
abkein <rickbatra0z@gmail.com>
2026-09-18 21:20:05 +0300

    universal: home: enable nix-direnv, disallow dotenv

--- universal/home-modules/shell.nix
+++ universal/home-modules/shell.nix
@@ -30,12 +30,13 @@
       silent = false;
       enableBashIntegration = true;
       enableZshIntegration = true;
-      # nix-direnv.enable = true;
+      nix-direnv.enable = true;
       config = {
         bash_path = "${config.programs.bash.package}/bin/bash";
         disable_stdin = true;
-        load_dotenv = true;
+        # load_dotenv = true;
         strict_env = true;
+        warn_timeout = "10s";
       };
     };
     micro = {
