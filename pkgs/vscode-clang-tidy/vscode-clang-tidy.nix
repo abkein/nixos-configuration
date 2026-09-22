4acd1625538fdd8fdfaa20a9e7b0da252cb36430
abkein <rickbatra0z@gmail.com>
2026-09-23 02:44:37 +0300

    pkgs: vscode-clang-tidy: use special builder

--- pkgs/vscode-clang-tidy/vscode-clang-tidy.nix
+++ pkgs/vscode-clang-tidy/vscode-clang-tidy.nix
@@ -1,20 +1,9 @@
 pkgs:
-pkgs.vscode-utils.buildVscodeExtension {
-  pname = "abkein.vscode-clang-tidy";
-  vscodeExtPublisher = "abkein";
-  vscodeExtName = "vscode-clang-tidy";
-  version = "0.7.0";
-  vscodeExtUniqueId = "abkein.vscode-clang-tidy";
-  src = ./clang-tidy-ab-kein-fork-0.7.0.vsix;
-
-  nativeBuildInputs = [ pkgs.unzip ];
-
-  unpackPhase = ''
-    runHook preUnpack
-    mkdir -p source
-    unzip -q "$src" -d source
-    # VSIX files normally contain an "extension/" top-level directory
-    sourceRoot="source/extension"
-    runHook postUnpack
-  '';
+pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+  vsix = ./clang-tidy-ab-kein-fork-0.7.0.vsix;
+  mktplcRef = {
+    name = "vscode-clang-tidy";
+    publisher = "abkein";
+    version = "0.7.0";
+  };
 }
